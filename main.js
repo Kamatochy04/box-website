4033202e217b35bc518434c2719d91eb47c7c2e2
Andrey Valiuk <andrey.valyuk.04@mail.ru>
2025-06-18 21:17:17 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const images = document.querySelectorAll(".background-slider__image");
+  let currentIndex = 0;
+
+  function changeBackground() {
+    // Убираем активный класс у текущего изображения
+    images[currentIndex].classList.remove("active");
+
+    // Переходим к следующему изображению
+    currentIndex = (currentIndex + 1) % images.length;
+
+    // Добавляем активный класс новому изображению
+    images[currentIndex].classList.add("active");
+  }
+
+  // Меняем изображение каждые 5 секунд
+  setInterval(changeBackground, 5000);
+
+  images[0].classList.add("active");
+
+  const loader = document.querySelector(".loader");
+
+  setTimeout(() => {
+    loader.classList.add("loader--hidden");
+
+    setTimeout(() => {
+      loader.remove();
+    }, 500);
+  }, 3000);
+});
